f074e046345581ace35f6676cba239654bc60dea
Alison Bruno <161540994+ABMS-NS@users.noreply.github.com>
2025-09-27 19:55:23 -0300

    init dos personagens mudado

--- models/personagem.py
+++ models/personagem.py
@@ -10,6 +10,11 @@ class Personagem:
         self.barras = [] #ela vai receber dicionarios para cada barra nova que for posta
         self.iniciativa = 0
         self.efeitos = [] # só recebe objetos da classe Efeito
+        # Estatísticas de combate (para cálculo de estatísticas gerais)
+        self.dano_recebido = 0
+        self.dano_causado = 0
+        self.cura_causada = 0
+        self.cura_recebida = 0
 
     def adicionar_barra(self, nome, atual, maximo):
         self.barras.append({"nome": nome, "atual": atual, "maximo": maximo})
